d434c1fd775642f732ab069704865911731f2031
jackfromillinois <yzc2@illinois.edu>
2023-04-22 10:31:20 -0500

    knight update

---
+++ GameManager.py
@@ -0,0 +1,234 @@
+import os, sys, argparse, timeit, random, warnings
+from typing import *
+from Logger import Logger, DebugLevel
+from TermArtist import TermArtist
+from Purge import Purge
+from Utils import PathFinder, DIRECTIONS_ALL, DIRECTIONS_ADJ, inBounds
+from Characters import *
+
+def twoplayers():
+    purge = Purge(12, 12, 3, 15)
+    print("======> Initial Map <======")
+    purge.showMap()
+
+    while True:
+        try:
+            # =============================
+            # Doctor
+            # =============================
+            # ◼︎ movement
+            docActionCnt = 1
+            while True:
+                v = input("(move doctor) w: top, s: south, a: left, d: right, x: sacrifice\n")
+                if v in ["w","s","a","d"]:
+                    purge.doctor.moveDirectionalByOne(v)
+                    purge.showMap()
+                    break
+                elif v == "x":
+                    docActionCnt += 1
+                    break
+                else:
+                    print("unknown command, try again...")
+            # ◼︎ action
+            while docActionCnt > 0:
+                v = input("(Doctor action) cc: cure cross, pn: place nurse: ")
+                if v == "cc":
+                    purge.doctor.cureCross()
+                    purge.showMap()
+                    docActionCnt -= 1
+                elif v == "pn":
+                    purge.doctor.placeNurse()
+                    purge.showMap()
+                    docActionCnt -= 1
+                else:
+                    print("unknown command, try again...")
+                
+                if docActionCnt == 0: break
+            
+            # =============================
+            # Kngiht
+            # =============================
+            # ◼︎ movement
+            knightActionCnt = 1
+            while knightActionCnt > 0:
+                v = input("(move knight) w: top, s: south, a: left, d: right, x: sacrifice\n")
+                if v in ["w","s","a","d"]:
+                    purge.knight.moveDirectionalByOne(v)
+                    purge.showMap()
+                    break
+                elif v == "x":
+                    knightActionCnt += 1
+                    break
+                else:
+                    print("unknown command, try again...")
+            # ◼︎ action
+            while True:
+                v = input("(Knight action) td: throw disinfectant, sw: switch position with doctor: ")
+                if v == "td":
+                    purge.knight.throwDisinfectant()
+                    purge.showMap()
+                    knightActionCnt -= 1
+                elif v == "sw":
+                    purge.knight.switchPositionWithDoctor()
+                    purge.showMap()
+                    knightActionCnt -= 1
+                else:
+                    print("unknown command, try again...")
+
+                if knightActionCnt == 0: break
+
+
+            # ========================================
+            # checking game condition and grow disease
+            # ========================================
+            status = purge.roundEnd()
+            print("Game Status:", status)
+            print("Disease Seed Count:", len(purge.diseaseSeeds))
+            
+            if status == 1:
+                print("you win!")
+                break
+            elif status == -1:
+                print("you lose!")
+                break
+
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as e:
+            print(e.with_traceback())
+
+
+
+def ai_knight():
+    purge = Purge(12, 12, 3, 15)
+    print("======> Initial Map <======")
+    purge.showMap()
+
+
+    while True:
+        try:
+            # =============================
+            # Doctor
+            # =============================
+            # ◼︎ movement
+            docActionCnt = 1
+            while True:
+                roads = input("(move doctor) w: top, s: south, a: left, d: right, x: sacrifice\n")
+                if roads in ["w","s","a","d"]:
+                    purge.doctor.moveDirectionalByOne(roads)
+                    purge.showMap()
+                    break
+                elif roads == "x":
+                    docActionCnt += 1
+                    break
+                else:
+                    print("unknown command, try again...")
+            # ◼︎ action
+            while docActionCnt > 0:
+                roads = input("(Doctor action) cc: cure cross, pn: place nurse: ")
+                if roads == "cc":
+                    purge.doctor.cureCross()
+                    purge.showMap()
+                    docActionCnt -= 1
+                elif roads == "pn":
+                    purge.doctor.placeNurse()
+                    purge.showMap()
+                    docActionCnt -= 1
+                else:
+                    print("unknown command, try again...")
+                
+                if docActionCnt == 0: break
+            
+
+            # =============================
+            # AI knight
+            # =============================
+            map, M, N = purge.map, purge.M, purge.N
+            knight = purge.knight
+
+            nearbyDisease = []
+            for i in range(1, 3):
+                for di, dj in DIRECTIONS_ALL:
+                    newPos = (knight.root[0]+di*i, knight.root[1]+dj*i)
+                    if not inBounds(*newPos, M, N): continue
+                    if isinstance(purge.peekCell(*newPos), Disease):
+                        nearbyDisease.append(newPos)
+                
+            if nearbyDisease:
+                targetPos = random.choice(nearbyDisease)
+                for DIFF in DIRECTIONS_ALL:
+                    neiPos = (targetPos[0]+DIFF[0], targetPos[1]+DIFF[1])
+                    if inBounds(*neiPos, M, N) and type(purge.peekCellBase(*neiPos)) == City:
+                        purge.map[neiPos[0]][neiPos[1]].noUpdateCnt = 4
+                if inBounds(*targetPos, M, N) and type(purge.peekCellBase(*targetPos)) == City:
+                    purge.map[targetPos[0]][targetPos[1]].noUpdateCnt = 4
+            
+            else:
+                while True:
+                    targetCity = random.choice(purge.cities)
+                    if targetCity.edgeDiseases:
+                        targetDisease = random.choice(targetCity.edgeDiseases)
+                        knight_to_disease = PathFinder.astar(map, knight.root, targetDisease.root, 
+                                                            [purge.doctor, *purge.diseaseSeeds, *purge.trees])
+                        if knight_to_disease: break
+                curCity = purge.peekCellBase(*knight.root)
+                print(knight_to_disease)
+                if knight_to_disease != -1:
+                    isInOtherCity = False
+                    for path in knight_to_disease:
+                        if purge.peekCellBase(*path):
+                            isInOtherCity = True
+                            break
+
+                    target_gate_pos = None
+                    if isInOtherCity:
+                        print("In Another City")
+                        for start_gate_pos, roads in curCity.roadsTo.items():
+                            for target_gate_path in roads:
+                                end_gate_pos = target_gate_path[0]
+                                if purge.peekCellBase(*end_gate_pos) == targetCity:
+                                    print("End gate:", end_gate_pos)
+                                    target_gate_pos = start_gate_pos if not purge.peekCellBase(*knight.root).isGate(*knight.root) else end_gate_pos
+                                    break
+                    print("Target gate", target_gate_pos)
+                    if target_gate_pos:
+                        if purge.peekCellBase(*knight.root).isGate(*knight.root):
+                            knight.moveTo(target_gate_pos)
+                        else:
+                            knight_to_disease = PathFinder.astar(map, knight.root, target_gate_pos, 
+                                                                [*purge.trees])
+                            print(knight_to_disease)
+                            knight.moveTo(knight_to_disease[1])
+            purge.showMap()
+
+            # ========================================
+            # checking game condition and grow disease
+            # ========================================
+            status = purge.roundEnd()
+            print("Game Status:", status)
+            print("Disease Seed Count:", len(purge.diseaseSeeds))
+            
+            if status == 1:
+                print("you win!")
+                break
+            elif status == -1:
+                print("you lose!")
+                break
+
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as e:
+            print(e.with_traceback())
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Purge")
+    parser.add_argument("-ai", "--ai_knight", type=bool, metavar="", default=False,
+                        action=argparse.BooleanOptionalAction, help="Start with an AI knight")
+    args = parser.parse_args()
+
+    if not args.ai_knight:
+        twoplayers()
+    elif args.ai_knight:
+        ai_knight()
---
+++ README.md
@@ -0,0 +1,53 @@
+<h1>Screenshots</h1>
+
+|Result                                          | Description                                                 |
+|------------------------------------------------| ------------------------------------------------------------|
+|<img src="./samples/12*12_3city.png" width="328px"></img>| A randomly generated map (12*12) with 3 cities in it |
+|<img src="./samples/disinfect.png" width="328px"></img>| Purple squares indicates cell that cannot be infected because of Knight's action |
+|<img src="./samples/nurse.png" width="328px"></img>| Doctor places a nurse |
+|<img src="./samples/swap_1.png" width="200px"></img><img src="./samples/swap_2.png" width="200px"></img>| Knight swapping position with Doctor |
+
+
+<h1>Introduction</h1>
+<p>This game is inspired by both board games—Pandemic and Monopoly. In this game, players have to work with each other and try to eradicate the diseases in all cities. A player would be either doctor or knight (in current version), and different specialties will have different skills to perform during each turn. The diseases win by spreading to all cities, and occupy more than 60% of the cells in each city. The players win by curing all diseases.</p>
+
+<h1>Game Rules</h1>
+<h2>Action</h2>
+<ul>
+    <li>
+        <h4>Doctor</h4>
+        <ul>
+            <li>
+                Can place a nurse in any adjacent cell (who can then cure infections in a 3x3 square centered around the position of the nurse)
+                <p>The nurse can cure infections in a 3x3 square centered around the position of the nurse. After performing this, the nurese will die</p>
+            </li>
+            <li>Can sacrifice movement for another action in the same turn</li>
+            <li>Can perform curing infections in the entire row and column according to doctor's current location</li>
+        </ul>
+    </li>
+    <li>
+        <h4>Knight</h4>
+        <ul>
+            <li>Can swap position with the doctor</li>
+            <li>Can perform throwing disinfectant two cells away from the knight's current position, the disinfectant can stops diseases from spreading for 2 whole rounds</li>
+        </ul>
+    </li>
+    <li>
+    <h4>Disease</h4>
+    <ul>
+        <li>At the end of each round, inside each city, all possible edge diseases can be picked as the root for disease spreading.</li>
+        <li>Disease can spread to another city if its current position is at a gate (red square)</li>
+        <li>Disease cannot grow to purple cell (cell that are disinfected)</li>
+    </ul>
+   </li>
+</ul>
+
+
+<h1>References</h1>
+<ol>
+   <li><a href="https://www.zmangames.com/en/games/pandemic/">Pandemic</a></li>
+   <li><a href="https://en.wikipedia.org/wiki/Monopoly_(game)">Monopoly</a></li>
+</ol>
+
+<h1>Old Repository</h1>
+<a href="https://github.com/jackfromillinois/Purge">https://github.com/jackfromillinois/Purge</a>
